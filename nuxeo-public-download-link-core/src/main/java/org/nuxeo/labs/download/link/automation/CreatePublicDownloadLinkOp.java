b677110354b3430f1b4ca9d42bc648e506214dea
michael <mvachette@nuxeo.com>
2021-03-03 13:42:50 +0900

    Fix automation

--- nuxeo-public-download-link-core/src/main/java/org/nuxeo/labs/download/link/automation/CreatePublicDownloadLinkOp.java
+++ nuxeo-public-download-link-core/src/main/java/org/nuxeo/labs/download/link/automation/CreatePublicDownloadLinkOp.java
@@ -28,6 +28,7 @@ import org.nuxeo.ecm.automation.core.annotations.Param;
 import org.nuxeo.ecm.core.api.Blob;
 import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.core.api.impl.blob.StringBlob;
 import org.nuxeo.labs.download.link.service.PublicDownloadLinkService;
 
 import java.util.Calendar;
@@ -35,11 +36,11 @@ import java.util.Calendar;
 @Operation(
         id = CreatePublicDownloadLinkOp.ID,
         category = Constants.CAT_DOCUMENT,
-        label = "DocuSign: Get Signed Blobs",
-        description = "Get the signed blobs for the given envelope ID")
+        label = "Get a public download url",
+        description = "Get a public download url")
 public class CreatePublicDownloadLinkOp {
 
-    public static final String ID = "DSGetSignedBlobsOp";
+    public static final String ID = "CreatePublicDownloadLink";
 
     @Context
     protected CoreSession session;
@@ -50,15 +51,20 @@ public class CreatePublicDownloadLinkOp {
     @Context
     protected PublicDownloadLinkService publicDownloadLinkService;
 
-    @Param(name = "begin", required = false, description = "ACE begin date.")
+    @Param(name = "xpath", required = false, description = "File xpath")
+    protected String xpath="file:content";
 
+    @Param(name = "begin", required = false, description = "ACE begin date.")
     protected Calendar begin;
+
     @Param(name = "end", required = false, description = "ACE end date.")
     protected Calendar end;
 
     @OperationMethod
     public Blob run(DocumentModel doc)  {
-        return null;
+        publicDownloadLinkService.setPublicDownloadPermission(doc);
+        session.saveDocument(doc);
+        return new StringBlob(publicDownloadLinkService.getPublicDownloadLink(doc,xpath));
     }
 
 }
